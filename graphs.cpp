adadfa9654c7102f829b8308abb07dbb3d2601a6
Kristine Zhang <Kristine@Kristines-MacBook-Air-2.local>
2017-02-24 21:40:55 -0500

    srsly poop

--- graphs.cpp
+++ graphs.cpp
@@ -1,81 +1,77 @@
 /* Creates different types of random graphs */
 #include "graphs.h"
 
-class Graph {
+Graph::Graph(int n, int dim) {
+	this->n = n;
+	this->dim = dim;
 
-	Graph(int n, int dim) {
-		this->n = n;
-		this->dim = dim;
+	srand(time(NULL));
 
-		srand(time(NULL));
-
-		if (dim == 0) {
-			for (int i=0; i<n; ++i) {
-				Vertex* v = malloc(sizeof(Vertex));
-				v->dim = dim;
-				v->coords.insert((float)i);
-				for (int j=0; j<n; j++) {
-					Vertex* u = &vertices[j];
-					Edge* e = malloc(sizeof(Edge));
-					e->u = *u;
-					e->v = *v;
-					e->length = static_cast <float> (rand());
-					edges.insert(*e);
-				}
-			}
-		}
-
+	if (dim == 0) {
 		for (int i=0; i<n; ++i) {
 			Vertex* v = malloc(sizeof(Vertex));
 			v->dim = dim;
-			for (int j=0; j<dim; ++j) {
-				v->coords.insert(static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
-			}
-
-			for (int j=0; j < vertices.size(); ++j) {
+			v->coords.insert((float)i);
+			for (int j=0; j<n; j++) {
 				Vertex* u = &vertices[j];
-				float sqsum = 0;
-				for (int d=0; d<dim; ++d) {
-					sqsum += pow((v->coords[d] - u->coords[d]),2);
-				}
 				Edge* e = malloc(sizeof(Edge));
 				e->u = *u;
 				e->v = *v;
-				e->length = sqrt(sqsum);
-				edges.insert(*e)
+				e->length = static_cast <float> (rand());
+				edges.insert(*e);
 			}
-
-			vertices.insert(*v);
 		}
 	}
 
-	void makeSet(Vertex x) {
-		x.parent = &x;
-		x.rank = 0;
-	}
-
-	Vertex* find(Vertex x) {
-		if (x != x.parent) {
-			x.parent = find(*x.parent);
+	for (int i=0; i<n; ++i) {
+		Vertex* v = malloc(sizeof(Vertex));
+		v->dim = dim;
+		for (int j=0; j<dim; ++j) {
+			v->coords.insert(static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
 		}
-		return *x.parent;
-	}
 
-	void combine(Vertex x, Vertex y) {
-		link(find(Vertex x),find(Vertex y));
-	}
-
-	void link(Vertex x, Vertex y) {
-		if (x.rank > y.rank) {
-			Vertex tmp = x;
-			x = y;
-			y = tmp;
-		}else if (x.rank == y.rank) {
-			y.rank++;
+		for (int j=0; j < vertices.size(); ++j) {
+			Vertex* u = &vertices[j];
+			float sqsum = 0;
+			for (int d=0; d<dim; ++d) {
+				sqsum += pow((v->coords[d] - u->coords[d]),2);
+			}
+			Edge* e = malloc(sizeof(Edge));
+			e->u = *u;
+			e->v = *v;
+			e->length = sqrt(sqsum);
+			edges.insert(*e)
 		}
-		x.parent = &y;
-		return y;
-	}
 
-};
+		vertices.insert(*v);
+	}
+}
+
+void Graph::makeSet(Vertex x) {
+	x.parent = &x;
+	x.rank = 0;
+}
+
+Vertex* Graph::find(Vertex x) {
+	if (x != x.parent) {
+		x.parent = find(*x.parent);
+	}
+	return *x.parent;
+}
+
+void Graph::combine(Vertex x, Vertex y) {
+	link(find(Vertex x),find(Vertex y));
+}
+
+void Graph::link(Vertex x, Vertex y) {
+	if (x.rank > y.rank) {
+		Vertex tmp = x;
+		x = y;
+		y = tmp;
+	}else if (x.rank == y.rank) {
+		y.rank++;
+	}
+	x.parent = &y;
+	return y;
+}
 
